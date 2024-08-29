a953dc0a0fa26830f530bd252f375ddb9a0c0a97
PerezJorge123 <perezgallojorgeivan@gmail.com>
2024-08-28 19:48:15 -0500

    agregar punto 4

--- MenuP.java
+++ MenuP.java
@@ -5,21 +5,25 @@ public class MenuP {
 
         int opt;
         Scanner sc = new Scanner(System.in);
-        Punto1 u = new Punto1();
-        Punto2 v = new Punto2();
-        Punto3 z = new Punto3();
+        Punto1 a = new Punto1();
+        Punto2 b = new Punto2();
+        Punto3 c = new Punto3();
+        Punto4 d = new Punto4();
         System.out.println("ingrese la opcion que desea /n 1. buscar producto 2. sumar cantidades 3. precio mayor");
         opt = sc.nextInt();
 
         switch (opt) {
             case 1:
-                u.EjecutarPunto1();
+                a.EjecutarPunto1();
                 break;
             case 2:
-                v.EjecutarPunto2();
+                b.EjecutarPunto2();
                 break;
             case 3:
-                z.EjecutarPunto3();
+                c.EjecutarPunto3();
+                break;
+            case 4:
+                d.EjecutarPunto4();
                 break;
             default:
                 System.out.println("ingrese una opcion valida");
